a6daff19ebb32a9ed43ebe8cbf82b08e805e1be2
Fabricio Floriano <f.florian0@hotmail.com>
2023-10-31 18:16:02 -0300

    Requisito 01?

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -1,6 +1,23 @@
-from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
+from src.models.ingredient import (
+    Ingredient,
+    Restriction,
+)  # noqa: F401, E261, E501
 
 
 # Req 1
 def test_ingredient():
-    pass
+    ingredients = Ingredient("queijo mussarela")
+    print(ingredients.restrictions)
+    assert ingredients.name == "queijo mussarela"
+    assert ingredients.restrictions == {
+        Restriction.LACTOSE,
+        Restriction.ANIMAL_DERIVED,
+    }
+    assert repr(ingredients) == "Ingredient('queijo mussarela')"
+
+    ingredients2 = Ingredient("ovo")
+    ingredients3 = Ingredient("queijo mussarela")
+    assert ingredients != ingredients2
+    assert ingredients == ingredients3
+
+    assert hash(ingredients) == hash(ingredients.name)
