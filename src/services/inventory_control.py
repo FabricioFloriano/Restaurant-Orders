d1fb158390198315797aa06d1c8ddc4b60aa161e
Fabricio Floriano <f.florian0@hotmail.com>
2023-11-02 15:51:27 -0300

    Requisito 05?

--- src/services/inventory_control.py
+++ src/services/inventory_control.py
@@ -27,8 +27,20 @@ class InventoryMapping:
 
     # Req 5.1
     def check_recipe_availability(self, recipe: Recipe) -> bool:
-        pass
+        for ingredient, amount in recipe.items():
+            if (
+                ingredient not in self.inventory
+                or self.inventory[ingredient] < amount
+            ):
+                return False
+        return True
 
     # Req 5.2
     def consume_recipe(self, recipe: Recipe) -> None:
-        pass
+        removal_stock = self.check_recipe_availability(recipe)
+        if removal_stock:
+            for ingredient, amount in recipe.items():
+                self.inventory[ingredient] -= amount
+            return
+        else:
+            raise (ValueError)
